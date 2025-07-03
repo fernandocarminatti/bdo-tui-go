89e81f790322c5fdec916cbd6443517b34cb9107
Fernando Carminatti <fernando.carminatti@gmail.com>
2025-07-03 14:58:39 -0300

    fix: tidying up some of the code.

--- main.go
+++ main.go
@@ -137,24 +137,25 @@ func fetchProfileData(profileURL string) tea.Cmd {
 		}
 
 		var profile FullProfile
-		infoBox := doc.Find("div.profile_detail")
-		profile.FamilyInfo.Name = strings.TrimSpace(infoBox.Find("div.nick_wrap > p.nick").Text())
-		profile.FamilyInfo.CreationDate = strings.TrimSpace(infoBox.Find("ul.line_list > li:nth-child(1) > span.desc").Text())
-		profile.FamilyInfo.Guild = strings.TrimSpace(infoBox.Find("ul.line_list > li > span.guild > a").Text())
-		profile.FamilyInfo.PAPD = strings.TrimSpace(infoBox.Find("ul.line_list > li:nth-child(3) > span.desc").Text())
-		profile.FamilyInfo.Energy = strings.TrimSpace(infoBox.Find("ul.line_list > li:nth-child(4) > span.desc").Text())
-		profile.FamilyInfo.Contribution = strings.TrimSpace(infoBox.Find("ul.line_list > li:nth-child(5) > span.desc").Text())
+		familyInfoBox := doc.Find("div.profile_detail")
+		profile.FamilyInfo.Name = strings.TrimSpace(familyInfoBox.Find("div.nick_wrap > p.nick").Text())
+		profile.FamilyInfo.CreationDate = strings.TrimSpace(familyInfoBox.Find("ul.line_list > li:nth-child(1) > span.desc").Text())
+		profile.FamilyInfo.Guild = strings.TrimSpace(familyInfoBox.Find("ul.line_list > li > span.guild > a").Text())
+		profile.FamilyInfo.PAPD = strings.TrimSpace(familyInfoBox.Find("ul.line_list > li:nth-child(3) > span.desc").Text())
+		profile.FamilyInfo.Energy = strings.TrimSpace(familyInfoBox.Find("ul.line_list > li:nth-child(4) > span.desc").Text())
+		profile.FamilyInfo.Contribution = strings.TrimSpace(familyInfoBox.Find("ul.line_list > li:nth-child(5) > span.desc").Text())
 		
-		lifeskillsData := doc.Find("ul.character_data_box")
-		lifeskillsData.Find("li").Each(func(i int, s *goquery.Selection) {
+		lifeskillsBox := doc.Find("ul.character_data_box")
+		lifeskillsBox.Find("li").Each(func(i int, s *goquery.Selection) {
 			name := strings.TrimSpace(s.Find("span.spec_name").Text())
-			rawLevel := strings.TrimSpace(s.Find("span.spec_level").Text())
-			mastery := strings.TrimSpace(s.Find("span.spec_stat").Text())
 
+			rawLevel := strings.TrimSpace(s.Find("span.spec_level").Text())
 			normalizedLevel := strings.Replace(rawLevel, "Nv.", " Nv.", 1)
 			normalizedSplit := strings.SplitN(normalizedLevel, " ", 2)
 			levelName := normalizedSplit[0]
 			levelInt := normalizedSplit[1]
+
+			mastery := strings.TrimSpace(s.Find("span.spec_stat").Text())
 
 			profile.LifeSkills = append(profile.LifeSkills, LifeSkill{
 				Name: name,
@@ -164,17 +165,18 @@ func fetchProfileData(profileURL string) tea.Cmd {
 			})
 		})
 
-		characterData := doc.Find("ul.character_list")
-		characterData.Find("li").Each(func(i int, s *goquery.Selection) {
+		charactersBox := doc.Find("ul.character_list")
+		charactersBox.Find("li").Each(func(i int, s *goquery.Selection) {
+
 			isMain := strings.Contains(s.Find("p.character_name span.selected_label").Text(), "Personagem Principal")
 			name := strings.TrimSpace(
 				s.Find("p.character_name").Contents().FilterFunction(func(i int, sel *goquery.Selection) bool {
 					return goquery.NodeName(sel) == "#text"
 				}).Text(),
 			)
+
 			class := strings.TrimSpace(s.Find("span.character_symbol em:nth-child(2)").Text())
 			level := strings.TrimSpace(s.Find("span.character_info span:nth-child(2)").Text())
-			
 			if isMain {
 				level += " Personagem Principal"
 			}
@@ -186,6 +188,7 @@ func fetchProfileData(profileURL string) tea.Cmd {
 			})
 		})
 		
+		// Debug .json file. Can append a listener to this file to grab content on every update. Could export too.
 		jsonData, err := json.MarshalIndent(profile, "", "  ")
 		if err != nil {
 			return errMsg{err}
@@ -216,11 +219,10 @@ func getDocument(url string) (*goquery.Document, error) {
 		return nil, fmt.Errorf("server returned status: %s", res.Status)
 	}
 
-
 	return goquery.NewDocumentFromReader(res.Body)
 }
 
-// --- (Init, Update, View) ---
+// --- Model "Workflow" ---
 
 func (m *model) Init() tea.Cmd {
 	return textinput.Blink
